8524d1910c143eba811eea134c5cc746c2be9032
Murali Krishna Mallela <mmallela9@gmail.com>
2025-11-11 23:28:18 +0530

    /api excluded from locale

--- src/middleware.ts
+++ src/middleware.ts
@@ -51,9 +51,6 @@ export default function middleware(req: NextRequest, event: NextFetchEvent) {
  */
 export const config = {
     matcher: [
-        // Skip Next.js internals and all static files, unless found in search params
-        '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-        // Always run for API routes
-        '/(api|trpc)(.*)',
+        '/((?!api|_next|.*\\..*).*)',
     ],
 };
